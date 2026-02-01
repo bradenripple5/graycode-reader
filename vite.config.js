8ca6a78648738de6e69226a145c06e5fccc572e5
bradenripple5 <121790279+bradenripple5@users.noreply.github.com>
2026-01-31 19:57:05 -0800

    currently trying to make radial lines radiating outward more stable, perhaps there is an obvious tranformation? i.e. that you could just see any sufficiently large section of the gray code regardless of the orientation and be able to essentially stretch it out and rotate it?

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite';
+import fs from 'node:fs';
 
 export default defineConfig({
   assetsInclude: ['**/*.html'],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    https: {
+      key: fs.readFileSync('./server.key'),
+      cert: fs.readFileSync('./server.crt'),
+    },
+  },
 });
